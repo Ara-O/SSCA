bf4679762925e0e40894726006de57819e322217
Ara <oladipoeyiara@gmail.com>
2023-06-13 13:15:05 -0400

    Setting up basic message sending with websockets

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
-const app = require("express")();
+const express = require("express");
 const { Server } = require("socket.io");
+const cors = require("cors");
+const app = express();
 const server = app.listen(8080);
+app.use(cors());
+app.use(express.json());
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
@@ -9,11 +13,20 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log("connected");
-  socket.on("test", (...args) => {
-    console.log("YAY");
+
+  socket.on("joinRoom", (data) => {
+    let roomId = `room-${data.roomId}`;
+    socket.join(roomId);
+    io.to(roomId).emit("confirmation", `User-test has joined this room`);
   });
+
+  socket.on("sendMessage", (data) => {
+    let roomId = `room-${data.roomId}`;
+    console.log(data);
+    io.to(roomId).emit("receiveMessage", data.message);
+  });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 });
-app.get("/", (req, res) => res.send("ee"));
--- ui/src/pages/chat.vue
+++ ui/src/pages/chat.vue
@@ -1,18 +1,51 @@
 <template>
   <h2>hi</h2>
+  <input type="text" v-model="userInput" />
+  <MainButton @click="test">Test</MainButton>
 </template>
 <script lang="ts" setup>
-import { useRoute } from "vue-router";
-import { onMounted } from "vue";
-import { io } from "socket.io-client";
+import MainButton from "../components/Buttons/MainButton.vue";
+import { useRoute, useRouter } from "vue-router";
+import { onMounted, ref } from "vue";
+// import axios from "axios";
+import { io, Socket } from "socket.io-client";
 
 const route = useRoute();
+const router = useRouter();
+let socket: Socket;
+let userInput = ref<string>("hola hola");
+let userId = "test";
+function test() {
+  socket.emit(`sendMessage`, {
+    roomId: route.params.id,
+    userId,
+    message: userInput.value,
+  });
+}
+
+function initializeSocketConnection(roomId: string) {
+  socket = io("http://localhost:8080");
+  socket.emit("joinRoom", {
+    roomId,
+    userId,
+  });
+
+  socket.on("confirmation", (data) => {
+    console.log("ROOM JOINED", data);
+  });
+
+  socket.on("receiveMessage", (message) => {
+    console.log("Message received", message);
+  });
+}
+
 onMounted(() => {
-  console.log(route.params.id);
-  if (route.params.id) {
-    const socket = io("http://localhost:8080");
-    console.log(socket);
-    socket.emit("test");
+  let roomId = route.params.id as string;
+  if (roomId) {
+    initializeSocketConnection(roomId);
+  } else {
+    alert("No room id");
+    router.push("/");
   }
 });
 </script>
